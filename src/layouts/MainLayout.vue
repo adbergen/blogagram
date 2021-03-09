00847302beb0a0a8cfd7285820f8d0659f603f18
Anthony Bergen <anthonydbergen@gmail.com>
2021-03-09 09:56:10 -0500

    updated header styling and made mobile responsive

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,29 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header class="bg-white text-grey-10" bordered>
-      <q-toolbar>
+      <q-toolbar class="constrain">
+        <q-btn
+          class="large-screen-only q-mr-sm"
+          flat
+          round
+          dense
+          icon="eva-camera-outline"
+          to="/camera"
+          size="18px"
+        />
+        <q-separator class="large-screen-only" vertical spaced />
         <q-toolbar-title class="text-grand-hotel text-bold"
           >Blogagram</q-toolbar-title
         >
+        <q-btn
+          class="large-screen-only"
+          flat
+          round
+          dense
+          icon="eva-home-outline"
+          to="/"
+          size="18px"
+        />
       </q-toolbar>
     </q-header>
 
@@ -36,9 +55,13 @@ export default {
 </script>
 
 <style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-sm-min)
+    height: 77px
 .q-toolbar__title
-  text-align: center
   font-size: 30px
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
 .q-footer
   .q-tab__icon
     font-size: 30px
